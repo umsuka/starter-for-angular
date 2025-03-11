ab91c3e4f3a115bcc67d75c85e76a08293efcf6a
Matej Bačo <matejbaco2000@gmail.com>
2025-03-11 13:13:57 +0000

    Fix webpack issue with Pink v1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import '@appwrite.io/pink';
-import '@appwrite.io/pink-icons';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
